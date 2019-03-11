60288f7309638fc7a2c0afb92551e5ab81f2ba4a
grace-mao23 <gmao00@stuy.edu>
2019-03-11 10:07:26 -0400

    Copied in partition

---
+++ Quick.java
@@ -0,0 +1,44 @@
+public class Quick {
+
+
+  public static int partition (int [] data, int start, int end) {
+   // generate random pivot index
+   int pivot = (int)(Math.random() * (end - start + 1)) + start;
+   // bring pivot element to index 0
+   int temp = data[start];
+   data[start] = data[pivot];
+   data[pivot] = temp;
+   // variables
+   pivot = 0;
+   start += 1;
+   //  System.out.println("O: " + Arrays.toString(data));
+   while (start != end) {
+     if (data[start] > data[pivot]) {
+       // switch start and end
+       int t = data[end];
+       data[end] = data[start];
+       data[start] = t;
+       end -= 1;
+     } else {
+       start += 1;
+     }
+   //  System.out.println(Arrays.toString(data));
+   }
+   // switching pivot to the right place
+   if (data[start] <= data[pivot]) {
+     int t = data[start];
+     data[start] = data[pivot];
+     data[pivot] = t;
+     pivot = start;
+   } else {
+     int t = data[start-1];
+     data[start-1] = data[pivot];
+     data[pivot] = t;
+     pivot = start-1;
+   }
+   //    System.out.println(Arrays.toString(data));
+   return pivot;
+ }
+
+
+}
